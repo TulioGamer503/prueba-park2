c1e599c78f2536189f9d9537f654569f0829e8ef
SJ262482 <jrsanchezjacinto72@gmail.com>
2024-08-16 10:12:15 -0600

    proyecto

--- prueba_datos/datos/prueba.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd "C:\xampp\htdocs\Crea-J-2024\prueba_datos\datos"
-python prueba2.py
-pause
--- prueba_datos/datos/prueba4.py
+++
@@ -1,6 +0,0 @@
-import serial, time
-ard = serial.Serial('COM3', 9600)
-datos = ard.readline()
-while 1:
-    print(datos)
-    time.sleep(3)
