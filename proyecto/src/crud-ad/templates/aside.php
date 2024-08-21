87a02cd98d64a50d87bbf62c3fadbebf4077c92c
SJ262482 <jrsanchezjacinto72@gmail.com>
2024-08-21 07:44:06 -0600

    aaaa
    aa

--- proyecto/src/crud-ad/templates/aside.php
+++ proyecto/src/crud-ad/templates/aside.php
@@ -46,7 +46,7 @@
                         </a>-->
                         <a href="../PHP/logout.php"
                             class="inline-flex items-center px-4 py-2 text-white bg-red-600 border border-transparent rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-                            Reportar Problema
+                            Cerrar Sesion
                         </a>
 
                     </div>
